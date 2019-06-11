f1d76a834c2afb2e2264c864e518e838ebce3bf2
RichardKGitHub <git.richardkummer@gmail.com>
2019-06-11 15:53:16 +0200

    fixed removel issues of _NoClass.py mod. Navigation and README

--- Navigation.py
+++ Navigation.py
@@ -1,1 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from unityagents import UnityEnvironment
 import torch
+import numpy as np
+
+#initialize Environment
+env = UnityEnvironment(file_name="/data/Banana_Linux_NoVis/Banana.x86_64")
+
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+
+# number of actions
+action_size = brain.vector_action_space_size
+
+# examine the state space
+state = env_info.vector_observations[0]
+state_size = len(state)
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ python3.6 \
 The Environment for this Project is included in the following Git-repository
 https://github.com/udacity/deep-reinforcement-learning.git
 - follow the given instructions to set up the Environment
-- copy the `python` folder into the `Navigation_Project` folder 
+- copy the included `python` folder into the `Navigation_Project` folder and make it a Sources Root <XXXX> 
 
 ####Packages
 conda install pytorch_gpu     #<XXXX> \
